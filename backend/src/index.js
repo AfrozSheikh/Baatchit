720fc39d30a33f67374700c9fa24d2aea60324dd
AfrozSheikh <sa8329925@gmail.com>
2025-05-25 12:33:21 +0530

    project done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,61 @@
-import express from "express";
-import { connectDB } from "./lib/db.js";
-const app = express() 
+// import express from "express";
+// import { connectDB } from "./lib/db.js";
+// const app = express() 
 
-import cookieParser from "cookie-parser";
-import authRoutes from './routes/auth.routes.js'
+// import cookieParser from "cookie-parser";
+// import authRoutes from './routes/auth.routes.js'
 
-app.use(express.json())
-connectDB() ;
-app.use(cookieParser())
-app.use("/api/auth", authRoutes)
+// app.use(express.json())
+// connectDB() ;
+// app.use(cookieParser())
+// app.use("/api/auth", authRoutes)
 
  
-app.listen(5001, ()=>{
-    console.log("hii");
+// app.listen(5001, ()=>{
+//     console.log("hii");
     
-})
+// })
+
+import express from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
